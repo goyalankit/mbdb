95a8dab114776d0f2f824053ed52a2d2e9410130
Ankit Goyal <ankitgoyal@utexas.edu>
2014-01-17 23:43:06 -0600

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -28,4 +28,48 @@ select pname from parts where price in (select min(price) from parts);
 select pname from parts where price <= all(select price from parts);
 
 -- query 10: What parts cost more than the most expensive Land Before Time part? (Hint: you should use pattern-matching, e.g., pname like 'Land Before Time%').
-select * from parts where pname like 'Land Before Time%';
+select * from parts where price > all(select max(price) from parts where pname in (select pname from parts where pname like 'Land Before Time%'));
+
+-- query 11: Write a correlated query to return the cities of zipcodes from which an order has been placed.
+select distinct(z1.city) from zipcodes z1 join customers c1 on z1.zip=c1.zip where c1.CNO in (select cno from orders o1 where o1.cno=c1.cno);
+
+-- query 12: Get cname values of customers who have placed at least one part order through employee with eno = 1000.
+select c1.cname from customers c1 where c1.cno in (select cno from orders where orders.ENO=1000);
+
+-- query 13: Get the total number of customers.
+select count(*) from customers;
+
+-- query 14: Get the pname values of parts that cost more than the average cost of all parts.
+select p1.pname from parts p1 where p1.price > all(select avg(price) from parts );
+
+-- query 15: For each part, get pno and pname values along with the total sales in dollars.--select ono, sum(qty) from odetails group by ono;
+select od1.pno, p1.pname, sum(od1.qty * p1.price) "total sales in $" from odetails od1 join parts p1 on od1.pno=p1.pno group by od1.pno, p1.pname;
+
+-- query 16: For each part, get pno and pname values along with the total sales in dollars, but only for total sales exceeding $200.
+select * from (select od1.pno, p1.pname, sum(od1.qty * p1.price) sales_total from odetails od1 join parts p1 on od1.pno=p1.pno group by od1.pno, p1.pname) where sales_total > 200;
+
+-- query 17: Repeat the last 2 queries, except this time create a view to simplify your work. Define the view and each query on that view.
+create view sales as select od1.pno, p1.pname, sum(od1.qty * p1.price) total_sales from odetails od1 join parts p1 on od1.pno=p1.pno group by od1.pno, p1.pname;
+select * from sales;
+select * from sales where total_sales > 200;
+
+-- query 18: Delete order 1021 and its order details.
+delete from odetails where ono=1021;
+delete from orders where ono=1021;
+
+-- query 19: Increase the cost of all parts by 5%.
+select * from parts;
+update parts set price=(price + 0.05 * price);
+
+-- query 20: Retrieve employees by name in reverse alphabetical order.
+select ename from employees order by ename desc;
+
+-- query 21: What tuples of Employees and Zipcodes do not participate in a join of these relations? Use the outerjoin and minus operations.
+-- To get the tuples of Zipcodes that doesn't participate in a join of these relations
+(select z1.* from employees e1 right outer join zipcodes z1 on e1.zip=z1.zip) minus (select z1.* from employees e1 join zipcodes z1 on e1.zip=z1.zip);
+
+-- To get the tuples of Employees that doesn't participate in a join of these relations
+(select e1.* from employees e1 left outer join zipcodes z1 on e1.zip=z1.zip) minus (select e1.* from employees e1 join zipcodes z1 on e1.zip=z1.zip);
+
+-- Single Query
+((select * from employees e1 right outer join zipcodes z1 on e1.zip=z1.zip) union (select * from employees e1 left outer join zipcodes z1 on e1.zip=z1.zip)) minus (select * from employees e1 join zipcodes z1 on e1.zip=z1.zip);
